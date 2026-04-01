4e9cd620515b71947b539698dd04a68be6577012
Alessandro Candelari <MOKA@Mac.fritz.box>
2026-04-01 15:39:09 +0200

    fix: enable CORS for production frontend

--- main.py
+++ main.py
@@ -57,7 +57,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://quiz-ui-ruddy.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
